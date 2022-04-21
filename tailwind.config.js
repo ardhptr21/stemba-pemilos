b742326132143a92b42e4b8cf8b94ab9308003b7
ardhptr21 <ardhiputrapradana21@gmail.com>
2022-04-21 09:35:20 +0700

    setup tailwindcss and alpinejs

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    content: ["./resources/**/*.blade.php"],
+    theme: {
+        extend: {
+            colors: {
+                primary: {
+                    bold: "#0e1734",
+                    light: "#2a3359",
+                    extralight: "#868dad",
+                },
+            },
+        },
+    },
+    plugins: [],
+};
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,17 +1,14 @@
-const mix = require('laravel-mix');
+const mix = require("laravel-mix");
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
- | file for the application as well as bundling up all the JS files.
- |
- */
+mix.js("resources/js/app.js", "public/js")
+    .postCss("resources/css/app.css", "public/css", [require("tailwindcss")])
+    .disableSuccessNotifications()
+    .browserSync({
+        proxy: "http://localhost:8000",
+        notify: false,
+        port: 8080,
+    });
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+if (mix.inProduction()) {
+    mix.version();
+}
