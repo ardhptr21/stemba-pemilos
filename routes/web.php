3ae3042328791888655a240c855fc95477e1738a
ardhptr21 <ardhiputrapradana21@gmail.com>
2022-08-31 10:24:43 +0700

    forcescheme https in production

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,11 @@
 <?php
 
+use Illuminate\Support\Facades\URL;
+
+if (env('APP_ENV') === 'production') {
+    URL::forceScheme('https');
+}
+
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CandidateController;
